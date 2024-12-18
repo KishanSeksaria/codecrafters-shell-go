ab65d0e5fc55cd4019e6d411c86832b7b2f095f8
Kishan Seksaria <kishanseksaria@gmail.com>
2024-12-18 23:06:36 +0530

    refactor main function to continuously prompt for user input

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,11 +9,11 @@ import (
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-
 	for {
+		fmt.Fprint(os.Stdout, "$ ")
+
+		// Wait for user input
+
 		inputCommand, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
 		fmt.Printf("%s: command not found\n", strings.TrimSpace(inputCommand))
