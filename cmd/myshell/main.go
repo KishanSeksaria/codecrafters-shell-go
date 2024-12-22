70e57894be294c13848e99f3181388a7f9e855f5
Kishan Seksaria <kishanseksaria@gmail.com>
2024-12-22 21:37:16 +0530

    refactor main function to improve command parsing and execution flow

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,48 +9,60 @@ import (
 	"strings"
 )
 
-func main() {
-	initCommands()
-	for {
-		fmt.Fprint(os.Stdout, "$ ")
-		// Wait for user input
-		in, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Printf("error reading from stdin: %s", err.Error())
-			os.Exit(1)
-		}
-		inputs := strings.Split(strings.TrimSpace(in), " ")
-		cmd := inputs[0]
-		args := inputs[1:]
-		cmdFn, ok := commands[cmd]
-		if !ok {
-			notFound(cmd)
-		} else {
-			cmdFn(args)
-		}
-	}
-}
-
 type (
 	cmdFnc func([]string)
 )
 
 var commands = make(map[string]cmdFnc)
 
+func main() {
+	// Initialize commands
+	initCommands()
+
+	// Main loop
+	for {
+		fmt.Print("$ ")
+
+		// Wait for user input
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Printf("error reading from stdin: %s", err.Error())
+			os.Exit(1)
+		}
+
+		// Parse input
+		inputs := strings.Split(strings.TrimSpace(input), " ")
+		inputCommand := inputs[0]
+		commandArguments := inputs[1:]
+
+		// Get command execution function
+		execute, ok := commands[inputCommand]
+		if !ok {
+			notFound(inputCommand)
+		} else {
+			execute(commandArguments)
+		}
+	}
+}
+
+// Function to register a command
 func registerCommand(cmd string, fn cmdFnc) {
 	commands[cmd] = fn
 }
 
+// Function to initialize commands
 func initCommands() {
 	registerCommand("exit", exit)
 	registerCommand("echo", echo)
 	registerCommand("type", typer)
 }
 
+// Function to handle command not found
 func notFound(cmd string) {
 	fmt.Printf("%s: command not found\n", cmd)
 }
 
+// Command functions: exit command
 func exit(args []string) {
 	if len(args) == 0 {
 		os.Exit(1)
@@ -60,27 +72,36 @@ func exit(args []string) {
 	}
 }
 
+// Command functions: echo command
 func echo(args []string) {
 	fmt.Println(strings.Join(args, " "))
 }
 
+// Command functions: type command
 func typer(args []string) {
 	if len(args) == 0 {
-		fmt.Println(os.Stdout, "")
-		fmt.Println("")
+		fmt.Println("type: usage: type <command>")
+		return
 	}
+
+	// Check if the command is a shell builtin
 	_, builtin := commands[args[0]]
 	if builtin {
 		fmt.Printf("%s is a shell builtin\n", args[0])
 		return
 	}
+
+	// Check if the command is in the PATH
 	paths := strings.Split(os.Getenv("PATH"), ":")
 	for _, path := range paths {
 		fp := filepath.Join(path, args[0])
+
+		// Check if the file exists
 		if _, err := os.Stat(fp); err == nil {
 			fmt.Println(fp)
 			return
 		}
 	}
+
 	fmt.Printf("%s: not found\n", args[0])
 }
